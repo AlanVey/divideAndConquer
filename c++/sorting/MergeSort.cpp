b44e5cc84675ab5595f81b2e02847e9a8213a5bf
Alan Vey <amv12@imperial.ac.uk>
2014-03-03 16:08:13 +0000

    initial attempt at merge_sort implementation

--- c++/sorting/MergeSort.cpp
+++ c++/sorting/MergeSort.cpp
@@ -19,5 +19,34 @@ void MergeSort::sortpart(std::vector<int> &A, int p, int r)
 /******************************************************/
 void MergeSort::merge(std::vector<int> &A, int p, int q, int r)
 {
-	/*********** INSERT YOUR CODE HERE ****************/
+	int len_sub_1 = q - p + 1;
+	int len_sub_2 = r - q;
+	std::vector<int> sub_1 (len_sub_1);
+	std::vector<int> sub_2 (len_sub_2);
+
+	for(int i = 0; i < len_sub_1; i++)
+	{
+		sub_1[i] = A[p + i - 1];
+	}
+	for(int i = 0; i < len_sub_2; i++)
+	{
+		sub_2[i] = A[q + j];
+	}
+	sub_1[len_sub_1 + 1] = INT_MAX;
+	sub_2[len_sub_2 + 1] = INT_MAX;
+	int i = 1;
+	int j = 1;
+	for(int k = p; k <= r; k++)
+	{
+		if(sub_1[i] <= sub_2[j])
+		{
+			A[k] = sub_1[i];
+			i++;
+		}
+		else
+		{
+			A[k] = sub_2[j];
+			j++;
+		}
+	}
 }
