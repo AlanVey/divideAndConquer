0832efc450928e3972620845f77e15f28eb759bd
Alan Vey <amv12@ic.ac.uk>
2014-03-05 19:47:43 +0000

    Sorting part of the task completed

--- c++/sorting/HybridSort.cpp
+++ c++/sorting/HybridSort.cpp
@@ -2,7 +2,7 @@
 #include "InsertionSort.hpp"
 #include "MergeSort.hpp"
 
-#define MERGE_VS_INSERT_TURNING_POINT 350
+#define TURNING_POINT 360
 
 void HybridSort::sort(std::vector<int> &A)
 {
@@ -14,18 +14,35 @@ void HybridSort::sort(std::vector<int> &A)
 /******************************************************/
 void HybridSort::sortpart(std::vector<int> &A, int p, int r)
 {
+
   if(p < r)
   {
-    if(r - p > MERGE_VS_INSERT_TURNING_POINT)
+    int q = (p + r)/2;
+    bool sub1 = q - p + 1 > TURNING_POINT;
+    bool sub2 = r - q     > TURNING_POINT;
+    if(sub1 && sub2)
     {
-      int q = (p + r)/2;
-      MergeSort::sortpart(A, p, q);
-      MergeSort::sortpart(A, q + 1, r);
+      sortpart(A, p, q);
+      sortpart(A, q + 1, r);
+      MergeSort::merge(A, p, q, r);
+    }
+    else if(!sub1)
+    {
+      InsertionSort::sortpart(A, p, q);
+      sortpart(A, q + 1, r);
+      MergeSort::merge(A, p, q, r);
+    }
+    else if(!sub2)
+    {
+      sortpart(A, p, q);
+      InsertionSort::sortpart(A, q + 1, r);
       MergeSort::merge(A, p, q, r);
     }
     else
     {
-      InsertionSort::sortpart(A, p, r);
-    }
+      InsertionSort::sortpart(A, p, q);
+      InsertionSort::sortpart(A, q + 1, r);
+      MergeSort::merge(A, p, q, r);
+    } 
   }
 }
